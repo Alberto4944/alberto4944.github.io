0b8b8586824885cf63b109d9ae842bbb7e7ee9bf
Alberto4944 <albertwu040@gmail.com>
2026-02-25 15:23:18 -0600

    weird

--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -5,59 +5,31 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let playerOneX = 100;
-let playerOneY = 100;
-let playerOneSize = 20;
-let playerOneSpeed=2;
+// COMPLETE CHANGE OF PLANS, make it a space avoider thing like my python final but have it switch inputs every so often
 
-let playerTwoX = 300;
-let playerTwoY = 300;
-let playerTwoSize = 20;
-let playerTwoSpeed=2;
+
 
 function setup() {
   noStroke();
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
+}
 
+let shipX = width/2;
+let shipY = 400;
+let shipSpeed = 5;
 
+function moveShipX() {
+  fill("black");
+  circle(shipX, shipY, 100);
+  if (keyIsDown(37)) {
+    shipX -= shipSpeed;
+  }
+  if (keyIsDown(39)) {
+    shipX += shipSpeed;
+  }
 }
 
 function draw() {
   background("white");
-  movePlayerOne();
-  movePlayerTwo();
-}
-
-function movePlayerOne() {
-  fill("red");
-  circle(playerOneX, playerOneY, playerOneSize);
-  if (keyIsDown(87)) {
-    playerOneY -= playerOneSpeed;
-  }
-  if (keyIsDown(83)) {
-    playerOneY += playerOneSpeed;
-  }
-  if (keyIsDown(65)) {
-    playerOneX -= playerOneSpeed;
-  }
-  if (keyIsDown(68)) {
-    playerOneX += playerOneSpeed;
-  }
-}
-
-function movePlayerTwo() {
-  fill("blue");
-  circle(playerTwoX, playerTwoY, playerTwoSize);
-  if (keyIsDown(38)) {
-    playerTwoY -= playerTwoSpeed;
-  }
-  if (keyIsDown(40)) {
-    playerTwoY += playerTwoSpeed;
-  }
-  if (keyIsDown(37)) {
-    playerTwoX -= playerTwoSpeed;
-  }
-  if (keyIsDown(39)) {
-    playerTwoX += playerTwoSpeed;
-  }
+  moveShipX();
 }
