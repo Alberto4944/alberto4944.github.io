fcbbb7633bcafbd5fd48e4f3028041bea5910622
Alberto4944 <albertwu040@gmail.com>
2026-03-05 15:18:47 -0600

    added a whole bunch (art and array object)

---
+++ array-object/sketch.js
@@ -0,0 +1,14 @@
+// Arrays and Object Notation
+// Albert Wu
+// March 5, 2026
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+}
--- index.md
+++ index.md
@@ -5,6 +5,8 @@
 - [Scope Demo](02-scope)
 - [Image Demo](03-images)
 - [Ball Array](04-ball-array)
+- [Generative Art](05-art)
 
 ## Assignments
 - [Interactive Scene](interactive-scene)
+- [Arrays and Object Notation](array-object)
