2e04e4dae1e024881505b5a936405e22c2d210e4
Alberto4944 <albertwu040@gmail.com>
2026-03-06 15:13:47 -0600

    followed the ml5 js tutorial for hand recognition

--- array-object/sketch.js
+++ array-object/sketch.js
@@ -5,10 +5,41 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+// Plan: Use ML5 to make a math game, where you have to put up a certain number of fingers to different questions (webcam)
+
+let handPose;
+let video;
+let hands = [];
+
+function preLoad() {
+  handPose = ml5.handPose();
+}
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(640, 480);
+  video = createCapture(VIDEO);
+  video.size(640, 480);
+  video.hide();
+
+  handPose.detectStart(video, gotHands);
 }
 
 function draw() {
   background(220);
+  image(video,0,0,width,height);
+
+  for (let i = 0; i < hands.length; i++) {
+    let hand = hands[i];
+    for (let j = 0; j < hands.keypoints.length; j++) {
+      let keypoint = hand.keypoints[j];
+      fill(0,255,0);
+      noStroke;
+      circle(keypoint.x, keypoint.y, 10);
+    }
+  }
+
+}
+
+function gotHands(results) {
+  hands = results;
 }
