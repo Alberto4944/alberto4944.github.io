b350e4faccf2975fb5f3ff5a69b8616588f421f4
Alberto4944 <albertwu040@gmail.com>
2026-03-16 15:24:27 -0600

    I got straight line detection to work, Just have not tested with camera. I used test points and it worked pretty well.

--- array-object/sketch.js
+++ array-object/sketch.js
@@ -11,6 +11,8 @@ let handPose;
 let video;
 let hands = [];
 
+let straightLineSize = 0.05;
+
 function preload() {
   handPose = ml5.handPose();
 }
@@ -19,30 +21,49 @@ function gotHands(results) {
   hands = results;
 }
 
-function setup() {
-  createCanvas(640, 480);
-  video = createCapture(VIDEO);
-  video.size(640, 480);
-  video.hide();
+let thePoints = [
+  {
+    x: 20,
+    y: 40
+  },
+  {
+    x: 20,
+    y: 40
+  },
+  {
+    x: 20,
+    y: 40
+  }
+];
 
-  handPose.detectStart(video, gotHands);
+function setup() {
+  // createCanvas(640, 480);
+  // video = createCapture(VIDEO);
+  // video.size(640, 480);
+  // video.hide();
+
+  // handPose.detectStart(video, gotHands);
+  console.log(isStraight(thePoints));
 }
+
 
 function draw() {
-  image(video,0,0,width,height);
+  // image(video,0,0,width,height);
 
-  if (hands.length > 0) {
-    let hand = hands[0];
-    let pointsArray = [hand.index_finger_tip, hand.index_finger_dip, hand.index_finger_pip, hand.index_finger_mcp];
-    noStroke();
-    fill(255,0,0);
-    for (point of pointsArray) {
-      circle(point.x, point.y, 16);
+  // if (hands.length > 0) {
+  //   let hand = hands[0];
+  //   let pointsArray = [hand.index_finger_tip, hand.index_finger_dip, hand.index_finger_pip, hand.index_finger_mcp];
+  //   noStroke();
+  //   fill(255,0,0);
+  //   for (point of pointsArray) {
+  //     circle(point.x, point.y, 16);
       
-    }
-    writeText(calculateAverage(pointsArray));
-  }
+  //   }
+  //   writeText(calculateAverage(pointsArray));
+  // }
 }
+
+
 
 function calculateAverage(pointsArray) {
   let average = 0;
@@ -54,7 +75,6 @@ function calculateAverage(pointsArray) {
 }
 
 function checkOne(average, pointsArray) {
-  let isOne = false;
   for (point of pointsArray) {
     if (point.x < average + 5 && point.x > average - 5) {
       return "ONE";
@@ -69,4 +89,17 @@ function writeText(state) {
   fill("black");
   textSize(40);
   text(`${state}`, width*0.8, height*0.95);
+}
+
+function isStraight(points) {
+  let firstPoint = [points[0].x, points[0].y];
+  let secondPoint = [points[1].x, points[1].y];
+  let slope = (secondPoint[1] - firstPoint[1]) / (secondPoint[0] - firstPoint[0]);
+  for (let i = 2; i < points.length; i++) {
+    newSlope = (points[i].y - firstPoint[1]) / (points[i].x - firstPoint[0]);
+    if (abs(newSlope-slope) > straightLineSize) {
+      return false;
+    }
+  }
+  return true;
 }
