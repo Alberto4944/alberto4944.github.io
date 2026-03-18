93fdc806f5b0925435b8f47ad06263b4fe4b68c1
Alberto4944 <albertwu040@gmail.com>
2026-03-18 15:20:46 -0600

    Added the grid demo from class.

---
+++ 07-grid/sketch.js
@@ -0,0 +1,42 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+let theGrid = [[1,0,0,0], 
+               [1,0,1,0],
+               [0,1,0,0],
+               [0,0,1,1]];
+const SQUARE_DIMENSION = theGrid.length;
+let cellSize;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  if (width>height) {
+    cellSize = height/SQUARE_DIMENSION;
+  }
+  else {
+    cellSize = width/SQUARE_DIMENSION;
+  }
+}
+
+function draw() {
+  background(220);
+  showGrid();
+}
+
+function showGrid() {
+  for (let y = 0; y < SQUARE_DIMENSION; y++) {
+    for (let x = 0; x < SQUARE_DIMENSION; x++) {
+      if (theGrid[y][x] === 1) {
+        fill("black");
+      }
+      else if (theGrid[y][x] === 0) {
+        fill("white");
+      }
+      square(x*cellSize, y*cellSize, cellSize);
+    }
+  }
+}
