95f16e1f2ac4a4b89115cf63af33d8afcde7d9ad
Alberto4944 <albertwu040@gmail.com>
2026-03-19 14:43:53 -0600

    Added extra for experts comment as well as overall cleanup. Thank you!

--- array-object/sketch.js
+++ array-object/sketch.js
@@ -1,13 +1,18 @@
 // Arrays and Object Notation
 // Albert Wu
-// March 5, 2026
+// March 19, 2026
 //
 // Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+// I used the ML5 library to track the points on the hand. This is a side-project to my Research Methods 20 class where I am researching how we can use AI and ML in table tennis coaching/training. 
 
 let handPose;
 let video;
 let hands = [];
+
+let fingerIndicator = {
+  color: "red",
+  size: 10
+};
 
 let theNumber = 0;
 
@@ -15,6 +20,8 @@ const STRAIGHT_LINE_MARGIN = 0.25; // How close the slopes can be to be consider
 const MINIMUM_DISTANCE_BETWEEN = 30; // This makes sure that bad detection coordinates do not get considered a line4
 const VIDEO_WIDTH = 640;
 const VIDEO_HEIGHT = 480;
+
+const TEXT_SIZE = 65;
 
 function preload() {
   handPose = ml5.handPose();
@@ -42,8 +49,8 @@ function draw() {
 function writeText() {
   // Writes the number on the screen
   fill("white");
-  textSize(50);
-  text(`${theNumber}`, width*0.8, height*0.95);
+  textSize(TEXT_SIZE);
+  text(`${theNumber}`, width*0.9, height*0.95);
 }
 
 function isStraight(points) {
@@ -63,7 +70,7 @@ function isStraight(points) {
 function drawPoints() {
   if (hands.length > 0) {
     let hand = hands[0];
-    fill("red");
+    fill(fingerIndicator.color);
   
     // These arrays make it easy to change what points should be shown and checked //
     let thumbPoints = [hand.thumb_cmc, hand.thumb_mcp, hand.thumb_ip, hand.thumb_tip];
@@ -75,7 +82,7 @@ function drawPoints() {
     // Draws The Points on the Video //
     for (fingers of [thumbPoints, indexPoints, middlePoints, ringPoints, pinkyPoints]) { 
       for (finger of fingers) {
-        circle(width - finger.x, finger.y, 10);
+        circle(width - finger.x, finger.y, fingerIndicator.size);
       }
     }
     // Below sets each finger to a true/false regarding if they are straight or not //
