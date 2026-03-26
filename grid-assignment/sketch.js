69553d548cae9448293c4be06d3f3391aea73395
Alberto4944 <albertwu040@gmail.com>
2026-03-26 15:21:25 -0600

    Got some more toggling working! My idea has shifted from a kenken generator to a more of displaying it and having the boards saved as json files. Will be more UI rather than the complicated math in creating boards.

--- grid-assignment/sketch.js
+++ grid-assignment/sketch.js
@@ -5,7 +5,7 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-// 2d rectangular grid demo
+// 2d rectangular operationGrid demo
 
 const ADD_SQUARE = "ADD";
 const MINUS_SQUARE = "MINUS";
@@ -13,44 +13,47 @@ const MULTIPLY_SQUARE = "MULTIPLY";
 const DIVIDE_SQUARE = "DIVIDE";
 
 const CELL_SIZE = 100;
-let rows;
-let cols;
-let grid;
+let rows = 5;
+let cols = 5;
+let operationGrid;
+let numberGrid;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  rows = Math.floor(height/CELL_SIZE);
-  cols = Math.floor(width/CELL_SIZE);
-  grid = generateRandomGrid(cols, rows);
+  // rows = Math.floor(height/CELL_SIZE);
+  // cols = Math.floor(width/CELL_SIZE);
+  operationGrid = generateRandomGrid(cols, rows);
 }
 
 function draw() {
   background(220);
   displayGrid();
+  // operationGrid[1][1] = ADD_SQUARE;
 }
 
 function keyPressed() {
   if (key === "r") {
-    grid = generateRandomGrid(cols, rows);
+    operationGrid = generateRandomGrid(cols, rows);
+    // findOthers(1,1);
   }
   else if (key === "e") {
-    grid = generateEmptyGrid(cols, rows);
+    operationGrid = generateEmptyGrid(cols, rows);
   }
 }
 
 function displayGrid() {
   for (let y = 0; y < rows; y++) {
     for (let x = 0; x < cols; x++) {
-      if (grid[y][x] === ADD_SQUARE) {
+      if (operationGrid[y][x] === ADD_SQUARE) {
         fill(244, 137, 137);
       }
-      else if (grid[y][x] === MINUS_SQUARE) {
+      else if (operationGrid[y][x] === MINUS_SQUARE) {
         fill("blue");
       }
-      else if (grid[y][x] === MULTIPLY_SQUARE) {
+      else if (operationGrid[y][x] === MULTIPLY_SQUARE) {
         fill("orange");
       }
-      else if (grid[y][x] === DIVIDE_SQUARE) {
+      else if (operationGrid[y][x] === DIVIDE_SQUARE) {
         fill("green");
       }
       square(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE);
@@ -63,13 +66,14 @@ function generateRandomGrid(cols, rows) {
   for (let y = 0; y < rows; y++) {
     newGrid.push([]);
     for (let x = 0; x < cols; x++) {
-      if (random(100) < 50) {
+      let randomNumber = random(100);
+      if (randomNumber > 75) {
         newGrid[y].push(ADD_SQUARE);
       }
-      else if (random(100) < 50) {
+      else if (randomNumber > 50) {
         newGrid[y].push(MINUS_SQUARE);
       }
-      else if (random(100) < 50) {
+      else if (randomNumber > 25) {
         newGrid[y].push(MULTIPLY_SQUARE);
       }
       else {
@@ -91,11 +95,41 @@ function generateEmptyGrid(cols, rows) {
   return newGrid;
 }
 
-function checkIfValid() {
-  for (let y = 0; y < rows; y++) {
-    for (let x = 0; x < cols; x++) {
-      neighbours = 0;
-      for (let side = 0; side < rows; )
-        for (let up = 0; up < cols; )
-          if (grid[side][up] === grid[y][x])
+function findOthers(x,y) {
+  // Choose direction for the math to go
+  if (x > 0 && x < cols && y > 0 && y < rows) {
+    let direction = Math.floor(random(0,5)); // 0 = North, 1 = East, 2 = South, 3 = West, 4 = Solo
+    console.log(direction);
+  }
+
+  // if (direction === 2) {
+
+  // }
+  // else if (x === 0 && direction === 3 || x === cols && direction === 1 || y === 0 && direction === 0 || y === rows && direction)
+}
+
+function mousePressed() {
+  let x = Math.floor(mouseX/CELL_SIZE);
+  let y = Math.floor(mouseY/CELL_SIZE);
+  console.log(x);
+  //self
+  toggleCell(x, y);
+}
+
+function toggleCell(x, y) {
+  //make sure the cell you're toggling is in the operationGrid
+  if (x >= 0 && x < cols && y >= 0 && y < rows) {
+    if (operationGrid[y][x] === ADD_SQUARE) {
+      operationGrid[y][x] = MINUS_SQUARE;
+    }
+    else if (operationGrid[y][x] === MINUS_SQUARE) {
+      operationGrid[y][x] = MULTIPLY_SQUARE;
+    }
+    else if (operationGrid[y][x] === MULTIPLY_SQUARE) {
+      operationGrid[y][x] = DIVIDE_SQUARE;
+    }
+    else {
+      operationGrid[y][x] = ADD_SQUARE;
+    }
+  }
 }
